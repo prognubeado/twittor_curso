e9bcf7929e9b76ec53809698eceb83422b6ea98e
Manuel Nubeado <programacion@nubeado.com>
2024-03-07 16:17:44 +0100

    Primer Commit

---
+++ js/sw-utils.js
@@ -0,0 +1,21 @@
+
+
+//GUARDAR EN EL CACHE DINAMICO
+function actualizaCacheDinamico(dynamicCache, req, res) 
+{
+
+    if (res.ok) {
+
+        caches.open(dynamicCache).then(cache => {
+
+            cache.put(req, res.clone());
+            return res.clone();
+
+        });
+
+    } else {
+        return res;
+    }
+
+}
+
